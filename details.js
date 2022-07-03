d1f7f90cf2384a82ef749276f9b03b3c851e2b31
WE4EVERKOMBAN <108579654+WE4EVERKOMBAN@users.noreply.github.com>
2022-07-03 10:46:50 +0530

    Update details.js

--- details.js
+++ details.js
@@ -1,5 +1,5 @@
 module.exports = {
-    token: '5561189249:AAGPUkFho_4N6_C22_WBwxm_X-ucoK50JhU',
+    token: '5577508068:AAHqheTp5bvIflxNZ_iIAN7RpPPawelGzik',
     admins: [895687774],
     curr:'INR'
 }
